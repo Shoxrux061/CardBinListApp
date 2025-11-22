a7b0c451f80b9915cc78b29180322f03afe69efe
Shoxrux061 <shoxruxurmanov2@gmail.com>
2025-11-22 18:20:43 +0500

    feat: add EmptyHistoryPlaceholder

--- feature_card-history/src/main/java/com/cardbin/feature_card_history/presentation/screens/card_history/CardHistoryScreen.kt
+++ feature_card-history/src/main/java/com/cardbin/feature_card_history/presentation/screens/card_history/CardHistoryScreen.kt
@@ -1,11 +1,14 @@
 package com.cardbin.feature_card_history.presentation.screens.card_history
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.cardbin.feature_card_history.data.mapper.toMap
 import com.cardbin.feature_card_history.domain.models.CardInfoUi
@@ -21,22 +24,27 @@ fun CardHistoryScreen(viewModel: CardHistoryViewModel) {
     val historyList = uiState.value.historyList
 
     when {
-
-        isLoading -> {
-            LoadingScreen()
-        }
-
-        error != null -> {
-            ErrorScreen(error) { }
-        }
-
-        historyList.isNotEmpty() -> {
-            CardHistoryContent(historyList)
-        }
-
+        isLoading -> LoadingScreen()
+        error != null -> ErrorScreen(error) {}
+        historyList.isEmpty() -> EmptyHistoryPlaceholder(modifier = Modifier.fillMaxSize())
+        else -> CardHistoryContent(historyList)
     }
 
 
+}
+
+@Composable
+fun EmptyHistoryPlaceholder(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "History is empty",
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.secondary
+        )
+    }
 }
 
 @Composable
